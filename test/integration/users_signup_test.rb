5b308de8d0ce9d75660fe66e4da0c2a9b6d64839
AlanViast <AlanViast@gmail.com>
2015-06-22 17:34:13 +0800

    Completion of Chapter 7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,6 +8,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, user: {name: "", email: "user@invalid", password:"foo", password_confirmation: "bar"}
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger'
   end
 
   test 'valid signup information' do
@@ -19,5 +21,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post_via_redirect users_path, user: { name: name, email: email, password: password, password_confirmation: password }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
