e7859d73127c3b064f5c85dfb99c1d3fc4a85403
AlanViast <AlanViast@gmail.com>
2015-06-25 00:00:06 +0800

    Completion of Chapter 9.2.2

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
+  end
+
+
+  test 'should get edit' do
+    get :edit , id: @user
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect update when not logged in' do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email}
+    assert_redirected_to login_url
   end
 
 end
--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -6,17 +6,26 @@ class UserEditTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "unsuccessful edit " do
+  test 'unsuccessful edit ' do
+    log_in_as(@user)
     get edit_user_path(@user)
-    patch user_path(@user), user: {name: '', email: 'foo@invalid', password: '', password_confirmation: ''}
-
+    patch user_path(@user), user: {name: '', email: 'foo@invalid', password: 'foo', password_confirmation: ''}
     assert_template 'users/edit'
-    assert_not flash.empty?
-    @user.reload
-    assert_redirected_to @user
+  end
 
+
+  test "successful edit " do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    name = "Foor bar"
+    email = "foo@bar.com"
+    patch user_path(@user), user: {name: name, email: email, password: '', password_confirmation: ''}
+
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
     assert_equal @user.name, name
-    assert_equal @user.name, email
+    assert_equal @user.email, email
   end
 
 end
